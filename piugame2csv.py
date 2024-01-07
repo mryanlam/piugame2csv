e514f21073987a2f6b35cc0ec4aff273bc1f1050
Matthew Lam <matt@mryanlam.com>
2024-01-06 17:12:46 -0800

    Format code with yapf

--- piugame2csv.py
+++ piugame2csv.py
@@ -7,14 +7,11 @@ import time
 import csv
 import logging
 
-
 base_url = "https://piugame.com/my_page/my_best_score.php?&&page="
 login_url = "https://piugame.com/bbs/login_check.php"
 login_page_url = (
-    "https://www.piugame.com/login.php?login_url=%2Fmy_page%2Fplay_data.php"
-)
+    "https://www.piugame.com/login.php?login_url=%2Fmy_page%2Fplay_data.php")
 creds = dict()
-
 
 cookies = {
     "sid": "n0ft722fdd8m69p6ba23t7oq55",
@@ -25,23 +22,39 @@ cookies = {
 }
 
 headers = {
-    "authority": "piugame.com",
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-language": "en-US,en;q=0.9",
-    "cache-control": "max-age=0",
-    "content-type": "application/x-www-form-urlencoded",
+    "authority":
+    "piugame.com",
+    "accept":
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language":
+    "en-US,en;q=0.9",
+    "cache-control":
+    "max-age=0",
+    "content-type":
+    "application/x-www-form-urlencoded",
     # 'cookie': 'sid=n0ft722fdd8m69p6ba23t7oq55; _ga=GA1.1.805729999.1692455102; PHPSESSID=n0ft722fdd8m69p6ba23t7oq55; 2a0d2363701f23f8a75028924a3af643=MTU0LjI3LjIxLjU4; _ga_D4HZW1SFFF=GS1.1.1693797062.7.0.1693797062.0.0.0',
-    "origin": "https://piugame.com",
-    "referer": "https://piugame.com/login.php?login_url=%2Fmy_page%2Fmy_best_score.php",
-    "sec-ch-ua": '"Chromium";v="116", "Not)A;Brand";v="24", "Microsoft Edge";v="116"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": '"Windows"',
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.69",
+    "origin":
+    "https://piugame.com",
+    "referer":
+    "https://piugame.com/login.php?login_url=%2Fmy_page%2Fmy_best_score.php",
+    "sec-ch-ua":
+    '"Chromium";v="116", "Not)A;Brand";v="24", "Microsoft Edge";v="116"',
+    "sec-ch-ua-mobile":
+    "?0",
+    "sec-ch-ua-platform":
+    '"Windows"',
+    "sec-fetch-dest":
+    "document",
+    "sec-fetch-mode":
+    "navigate",
+    "sec-fetch-site":
+    "same-origin",
+    "sec-fetch-user":
+    "?1",
+    "upgrade-insecure-requests":
+    "1",
+    "user-agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.69",
 }
 
 plate_mapping = {
@@ -100,7 +113,8 @@ def parse_best_score(page_content: bs.element.Tag):
                 song_name = "Cross Ray"
             score["Song"] = song_name
 
-            score_value = int(li.find("span", class_="num").text.replace(",", ""))
+            score_value = int(
+                li.find("span", class_="num").text.replace(",", ""))
             # print(f"score: {score_value}")
             score["Score"] = score_value
             parsed_scores.append(score)
@@ -142,7 +156,7 @@ def parse_best_scores(page_text: str, s: requests.Session):
     best_scores.extend(cur_page_scores)
 
     # for page_num in range(2, 3):
-    for page_num in range(2, last_page+1):
+    for page_num in range(2, last_page + 1):
         time.sleep(1)
         cur_page_url = base_url + str(page_num)
         print(cur_page_url)
@@ -153,12 +167,14 @@ def parse_best_scores(page_text: str, s: requests.Session):
         best_scores.extend(cur_page_scores)
     return best_scores
 
+
 def output_csv(scores):
     with open("scores.csv", "w") as csvfile:
-        fields = ["Song","Difficulty","Score","LetterGrade","Plate"]
+        fields = ["Song", "Difficulty", "Score", "LetterGrade", "Plate"]
         writer = csv.DictWriter(csvfile, fieldnames=fields)
         writer.writeheader()
         writer.writerows(scores)
+
 
 def difficulty_values(diff: str) -> [str, int]:
     chart_type = ""
@@ -175,6 +191,7 @@ def difficulty_values(diff: str) -> [str, int]:
 
     return (chart_type, chart_level)
 
+
 def post_piuscores(scores, creds):
     piuscores_arroweclipse_uri = "https://piuscores.arroweclip.se/api/phoenixScores"
     for row in scores:
@@ -188,9 +205,13 @@ def post_piuscores(scores, creds):
         json_payload["score"] = int(row["Score"])
         json_payload["isBroken"] = False
 
-        res = requests.post(piuscores_arroweclipse_uri, json=json_payload, auth=(creds["piuscores_user"], creds["piuscores_key"]))
+        res = requests.post(piuscores_arroweclipse_uri,
+                            json=json_payload,
+                            auth=(creds["piuscores_user"],
+                                  creds["piuscores_key"]))
         if not res.ok:
             print(f"Failed to post: {json_payload}")
+
 
 if __name__ == "__main__":
     # load creds
